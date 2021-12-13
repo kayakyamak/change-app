7645e159f70de49f261c3c4ed92cb65488b2681a
EC2 Default User <ec2-user@ip-172-31-39-116.us-east-2.compute.internal>
2021-12-13 10:31:32 +0000

    finished change flask app

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+def change(amount):
+    # calculate the resultant change and store the result to res
+    res = []
+    coins = [1, 5, 10, 25]  # values of pennies, nickels, dimes, and quarteres
+    coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
+    
+    # divide the amount*100 (the amount in cents) by a coin value
+    # record the number of coins that evenly divide and the remainder
+    coin = coins.pop()
+    num, rem = divmod(int(amount*100), coin)
+    # append the coin type and number of coins that had no remainder
+    res.append({num:coin_lookup[coin]})
+    
+    # while there is still some remainder, continue adding coins to the result
+    while rem > 0:
+        coin = coins.pop()
+        num, rem = divmod(rem, coin)
+        if num:
+                if coin in coin_lookup:
+                    res.append({num:coin_lookup[coin]})
+                    
+    return res
+    
+@app.route('/')
+def helo():
+    """Return a firendly HTTP greeting"""
+    print("I am inside hello world")
+    return 'Hello World! I can make change at route: /change'
+    
+@app.route('/change/<dollar>/<cents>')
+def changeroute(dollar, cents):
+    print(f"Make change for {dollar}.{cents}")
+    amount = f"{dollar}.{cents}"
+    result = change(float(amount))
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
